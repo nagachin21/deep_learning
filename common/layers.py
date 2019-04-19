8326bece086c491006eabcb65cfe90d2e1f2d8e6
wakayama <y.wakayama21.43@gmail.com>
2019-04-20 08:46:04 +0900

    add layers

--- common/layers.py
+++ common/layers.py
@@ -27,3 +27,39 @@ class sigmoid:
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
         return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.x) + self.b
+        return out
+
+    def backward(self, dout):
+        self.db = np.sum(dout, axis=0)
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        return dx
+
+class  SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.x, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
