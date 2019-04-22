9a2f858e38a529f12dbfa307629913d52e43afad
wakayama <y.wakayama21.43@gmail.com>
2019-04-22 09:10:23 +0900

    modify multi_layer_net

--- common/multi_layer_net.py
+++ common/multi_layer_net.py
@@ -1,1 +1,79 @@
 import numpy as np
+from collection import OrderedDict
+from common.layers import *
+from common.gradient import numerical_gradient
+
+class MultiLayerNet:
+    """全結合による多層ニューラルネットワーク
+
+    Parameters
+    ----------
+    input_size : 入力サイズ（MNISTの場合は784）
+    hidden_size_list : 隠れ層のニューロンの数のリスト(e.g. [100, 100, 100])
+    output_size : 出力サイズ（MNISTの場合は10）
+    activation : 'ReLU' or 'sigmoid'
+    weight_init_std : 重みの標準偏差を指定(e.g. 0.01)
+        'relu'または'he'を指定した場合は「Heの初期値」を設定
+        'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
+    weight_decay_lambda : Weight Decay（L2ノルム）の強さ
+    """
+
+    def __init__(self, input_size, hidden_size_list, output_size,
+                 activation='relu', weight_init_std='relu', weight_decay_lambda=0):
+         self.input_size = input_size
+         self.output_size = output_size
+         self.hidden_size_list = hidden_size_list
+         self.hidden_layer_num = len(hidden_size_list)
+         self.weight_init_std = weight_init_std
+         self.weight_decay_lambda = weight_decay_lambda
+         self.params = {}
+
+         #重みの初期化
+         self.__init__weight(weight_init_std)
+
+         #レイヤの生成
+         activation_layer = {'sigmoid': Sigmoid, 'relu':Relu}
+         self.layers = OrederdDict()
+         for idx in range(1, self.hidden_layer_num+1):
+             self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
+                                                       self.params['b' + str(idx)])
+             self.layers['Activation_function' + str(idx)] = activation_layer[activation]()
+
+         idx = self.hidden_layer_num + 1
+         self.layers['Affine' + str(idx)] = Affine(self.params['W' + str(idx)],
+                                                   self.params['b' + str(idx)])
+         self.last_layer = SoftmaxWithLoss()
+
+    def __init_weight(self, weight_init_std):
+        """重みの初期設定
+
+        Parameters
+        ----------
+        weight_init_std : 重みの標準偏差を指定(e.g. 0.01)
+           'relu'または'he'を指定した場合は「Heの初期値」を設定
+           'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
+        """
+        all_size_list = [self.input_size] + self.hidden_size_list + [self.output_size]
+        for idx in range(1, len(all_size_list)):
+            scale = weight_init_std
+            if str(weight_init_std).lower() in ('relu', 'he'):
+                scale = np.sqrt(2.0 / all_size_list[idx - 1]) #ReLUを利用する際に推奨される初期値
+            elif str(weight_init_std).lower() in ('sigmoid', 'xavier'):
+                scale = np.sqrt(1.0 / all_size_list[idx - 1]) #sigomidを利用する際に推奨される初期値
+
+            self.params['W' + str(idx)] = scale * np.random.randn(all_size_list[idx - 1], all_size_list)
+            self.params['b' + str(idx)] = scale * np.zeros(all_size_list)
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forwrad(x)
+        return x
+
+    def loss(self, x, t):
+        """
+        損失関数を求める
+
+        Parameters
+        ----------
+        
+        """
