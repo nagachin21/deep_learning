5cf87a751440d8aa825d7cee14c019fc3cfa9a4d
wakayama <y.wakayama21.43@gmail.com>
2019-04-25 09:04:21 +0900

    forgot adding this file

--- common/optimizer.py
+++ common/optimizer.py
@@ -40,7 +40,7 @@ class AdaGrad:
             params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
 
 class Adam:
-    def __init__(self, lr=0.01, beta1=0.9, beta2=0.999):
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
@@ -56,10 +56,10 @@ class Adam:
                 self.v[key] = np.zeros_like(val)
 
         self.iter += 1
-        lr_t = self.lr * np.sqrt(1 - self.beta2**self.iter) / (1- self.beta1**self.iter)
+        lr_t = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)
 
         for key in params.keys():
             self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
             self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
 
-            params[key] -= lr_t * self.m[key] / (np.sqrt(1 - self.v[key]) + 1e-7)
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
