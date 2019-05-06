38375eccdb98fe400e11387d9648a5d5098fba16
nagachin21 <y.wakayama21.43@gmail.com>
2019-05-06 15:21:21 +0900

    modify layer and util

--- common/layers.py
+++ common/layers.py
@@ -64,3 +64,27 @@ class  SoftmaxWithLoss:
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
         return dx
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FW, FH = self.W.shape
+        N, C, H, W    = self.x.shape
+        out_h = int(1 + (H + self.pad * 2 - FH) / self.stride)
+        out_w = int(1 + (W + self.pad * 2 - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transponse(0, 3, 1, 2)
+
+        return out
+
+    def backward(self, dout):
+        return 
--- common/util.py
+++ common/util.py
@@ -10,3 +10,34 @@ def smooth_curve(x):
     w = np.kaiser(window_len, 2)
     y = np.convolve(w/w.sum(), s, mode='valid')
     return y[5:len(y)-5]
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """
+
+    Parameter
+    ---------
+    input_data: （データ数,チャンネル数,高さ,幅）の4次元配列からなる入力データ
+    filter_h  : フィルターの高さ
+    filter_w  : フィルターの幅
+    stride    : ストライド
+    pad       : パディング
+
+    Returns
+    -------
+    col : 2次元配列
+    """
+    N, C, H, W = input_data.shape
+    out_h = int(1 + (H + pad * 2 - filter_h) / stride)
+    out_w = int(1 + (W + pad * 2 - filter_w) / stride)
+
+    img = np.pad(input_data, [(0,0), (0,0), (pad, pad)], 'constant')
+    col = np.zeros(N, C, filter_h, filter_w, out_h, out_w)
+
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transponse(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+    return col 
