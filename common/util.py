b6baae50de1618b38eeecacd7c9273c6229548bc
nagachin21 <y.wakayama21.43@gmail.com>
2019-05-06 17:56:12 +0900

    modify layers and util

--- common/util.py
+++ common/util.py
@@ -39,5 +39,35 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
             x_max = x + stride * out_w
             col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
 
-    col = col.transponse(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
-    return col 
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+    return col
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """
+
+    Parameters
+    ----------
+    col:
+    input_shape: 入力データの形状:(ex. [10, 1, 28, 28])
+    filter_h:
+    filter_w:
+    stride:
+    pad:
+
+    Returns
+    -------
+    img
+    """
+    N, C, H, W = input_shape
+    out_h = int(1 + (H + pad * 2 - filter_h) / stride)
+    out_w = int(1 + (W + pad * 2 - filter_w) / stride)
+    col = col.reshape(N, out_h, out_w, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H+2*pad+stride-1, W+2*pad+stride-1))
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
