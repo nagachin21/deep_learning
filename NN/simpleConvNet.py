cd2eb853edb2e758cc2d21dd807a704f520da1f6
nagachin21 <y.wakayama21.43@gmail.com>
2019-05-11 07:58:23 +0900

    add new file

---
+++ NN/simpleConvNet.py
@@ -0,0 +1,36 @@
+class SimpleConvNet:
+    """単純なConvNet
+
+    conv - relu - pool - affine - relu - affine - softmax
+
+    Parameters
+    ----------
+    input_size : 入力サイズ（MNISTの場合は784）
+    hidden_size_list : 隠れ層のニューロンの数のリスト（e.g. [100, 100, 100]）
+    output_size : 出力サイズ（MNISTの場合は10）
+    activation : 'relu' or 'sigmoid'
+    weight_init_std : 重みの標準偏差を指定（e.g. 0.01）
+        'relu'または'he'を指定した場合は「Heの初期値」を設定
+        'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
+    """
+    def __init__(self, input_dim={1, 28, 28},
+                 conv_param={'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+                 hidden_size=100, output_size=10, weight_init_std=0.01):
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2 * filter_pad) / filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size / 2) * (conv_output_size / 2))
+
+        #  重みの初期化
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        
